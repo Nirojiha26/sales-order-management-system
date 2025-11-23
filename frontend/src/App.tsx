0143ffb27b73345c2bfb3b20a91a2d905119da44
Nirojiha <nironirojiha@gmail.com>
2025-11-23 19:27:42 +0530

    Ensure route for print

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SalesOrderPage from "./pages/SalesOrderPage";
+import PrintInvoicePage from "./pages/PrintInvoicePage";
+
 
 export default function App() {
   return (
@@ -9,6 +11,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/order" element={<SalesOrderPage />} />
         <Route path="/sales-order" element={<SalesOrderPage />} />
+<Route path="/sales-order/:id/print" element={<PrintInvoicePage />} />
       </Routes>
     </BrowserRouter>
   );
